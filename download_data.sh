ed6b67bcfbce796a38d4bba538e598a47f289358
Matteo Barbieri <matteo.a.barbieri@gmail.com>
2020-11-01 13:02:08 +0100

    Add code to download also font

--- download_data.sh
+++ download_data.sh
@@ -6,3 +6,10 @@ mkdir -p models
 # Download model from dropbox
 wget https://www.dropbox.com/s/t0i4x9bgaml3nra/resnet101_allfonts_mnist.pth?dl=1 \
     -O models/resnet101_allfonts_mnist.pth
+
+# Create folder for fonts
+mkdir -p fonts
+
+# Download font from dropbox
+wget https://www.dropbox.com/s/wad32grfxvhtamp/Helvetica-Bold-Font.ttf?dl=1 \
+    -O fonts/Helvetica-Bold-Font.ttf
